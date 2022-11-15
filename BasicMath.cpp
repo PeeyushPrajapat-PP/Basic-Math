9b8c73569b8ffbbbd73c659a26a3257c2b032166
PeeyushPrajapat-PP <63343694+PeeyushPrajapat-PP@users.noreply.github.com>
2022-11-15 11:15:38 +0000

    Add files via upload

---
+++ BasicMath.cpp
@@ -0,0 +1,26 @@
+// a simple c++ program demonstrating how to use the basic mathematical operators add, subtract, multiply and divide
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int n1,n2, addition = 0, subtraction = 0, multiplication=0, division=0;
+
+	cout << "Enter a number: "; cin > >n1;
+	cout << "Enter another number: "; 
+	cin >> n2;
+
+	sum = n1 + n2;
+	subtraction = n1 - n2;
+	multiplication = n1 * n2;
+	division = n1 / n2;
+
+	cout << "\ The sum is: "<< sum <<endl;
+	cout << "The subtraction is: "<< subtraction <<endl;
+	cout << "The multiplication is: "<< multiplication <<endl;
+	cout << "The division is: "<< division <<endl;
+
+	return 0;
+
+}
